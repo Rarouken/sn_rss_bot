8c97327b9ca86d153940b95703edc1be42a40304
Rarouken <rarouken@gmail.com>
2025-07-09 22:37:48 +0200

    Update rss_reader.py

--- rss_reader.py
+++ rss_reader.py
@@ -5,6 +5,19 @@ import os
 import time
 from transformers import pipeline
 import logging
+import yaml
+
+def load_config(path="config.yaml"):
+    with open(path, "r", encoding="utf-8") as f:
+        config = yaml.safe_load(f)
+    return config
+
+config = load_config()
+
+rss_feeds = config["rss_feeds"]
+LT_ENDPOINTS = config["lt_endpoints"]
+DISCORD_WEBHOOK = config["discord_webhook"]
+SLAVIC_COUNTRIES = config["slavic_countries"]
 
 logging.basicConfig(
     level=logging.INFO,  # Zmień na DEBUG jeśli chcesz więcej szczegółów
@@ -12,111 +25,6 @@ logging.basicConfig(
     datefmt='%H:%M:%S'
 )
 logger = logging.getLogger(__name__)
-
-
-SLAVIC_COUNTRIES = [
-    # Polska
-    "polska", "poland", "pl", "warszawa", "warsaw", "kraków", "krakow", "wrocław", "wroclaw", "gdańsk", "gdansk", "poznan", "poznan", "lodz", "łódź", "szczecin", "katowice", "lubin",
-    # Czechy ...
-    # ... reszta jak poprzednio
-]
-
-# --- Lista RSS-ów ---
-rss_feeds = [
-    # 📌 Ogólne źródła informacyjne
-    "https://www.rmf24.pl/fakty/polska/feed",                # Polska – RMF24
-    "https://rss.onet.pl/",                                 # Polska – Onet
-    "https://gazeta.pl/rss/",                               # Polska – Gazeta.pl
-
-    "https://www.ceskatelevize.cz/rss/ct24.xml",            # Czechy – ČT24
-    "https://www.idnes.cz/rss.aspx",                        # Czechy – iDnes.cz
-    "https://rss.aktualne.cz/",                             # Czechy – Aktuálně.cz
-
-    "https://www.sme.sk/rss/",                              # Słowacja – SME.sk
-    "https://spravy.pravda.sk/rss/",                        # Słowacja – Pravda.sk
-    "https://www.aktuality.sk/rss/",                        # Słowacja – Aktuality.sk
-
-    "https://meduza.io/rss/all",                            # Rosja – Meduza
-    "https://rss.ria.ru/export/rss2/world/index.xml",       # Rosja – RIA Novosti
-    "https://www.themoscowtimes.com/rss",                   # Rosja – The Moscow Times
-
-    "https://www.pravda.com.ua/rss",                        # Ukraina – Ukrainska Pravda
-    "https://kyivindependent.com/feed",                     # Ukraina – Kyiv Independent
-    "https://www.kyivpost.com/rss/site.xml",                # Ukraina – Kyiv Post
-    "https://www.radiosvoboda.org/api/feeds",               # Ukraina – Radio Svoboda
-
-    "https://nashaniva.com/ru/rss/",                        # Białoruś – Nasha Niva
-    "https://www.rferl.org/api/feeds?region=Belarus",       # Białoruś – Radio Svoboda
-
-    "https://www.rts.rs/rss/ci.xml",                        # Serbia – RTS
-    "https://www.blic.rs/rss",                              # Serbia – Blic
-    "https://www.danas.rs/feed/",                           # Serbia – Danas
-
-    "https://www.hrt.hr/feeds/",                            # Chorwacja – HRT
-    "https://www.index.hr/rss/",                            # Chorwacja – Index.hr
-    "https://www.jutarnji.hr/rss",                          # Chorwacja – Jutarnji list
-
-    "https://balkaninsight.com/feed/",                      # Bośnia i Hercegowina – BIRN BiH
-    "https://cin.ba/feed/",                                 # Bośnia i Hercegowina – CIN
-    "https://ba.n1info.com/rss/",                           # Bośnia i Hercegowina – N1 BiH
-
-    "https://www.vijesti.me/rss",                           # Czarnogóra – Vijesti
-    "https://www.raskrinkavanje.me/rss",                    # Czarnogóra – Raskrinkavanje
-
-    "https://kallxo.com/feed/",                             # Kosowo – Kallxo
-    "https://www.koha.net/rss",                             # Kosowo – Koha Ditore
-
-    "https://meta.mk/feed/",                                # Macedonia Północna – Meta.mk
-    "https://www.slobodenpecat.mk/feed/",                   # Macedonia Północna – Sloboden Pechat
-
-    "https://btvnovinite.bg/rss",                           # Bułgaria – BTV News
-    "https://nova.bg/news/rss",                             # Bułgaria – Nova TV
-    "https://www.dnevnik.bg/rss.php",                       # Bułgaria – Dnevnik.bg
-
-    "https://www.rtvslo.si/rss/",                           # Słowenia – RTV Slovenija
-    "https://www.delo.si/rss/",                             # Słowenia – Delo
-    "https://podcrto.si/feed/",                             # Słowenia – Pod črto
-
-    # 💼 Sekcje ekonomiczne
-    "https://www.pb.pl/rss",                                # Polska – Puls Biznesu :contentReference[oaicite:1]{index=1}
-
-    "https://hn.ihned.cz/rss",                              # Czechy – Hospodářské noviny
-
-    "https://www.hnonline.sk/rss",                          # Słowacja – Hospodárske noviny
-
-    "https://www.vedomosti.ru/rss/news.xml",                # Rosja – Vedomosti
-
-    "https://www.interfax.com.ua/ua/rss/business/",         # Ukraina – Interfax-Ukraine Business
-
-    # Białoruś – brak RSS specyficznego, można sięgnąć po emigracyjne
-    "https://bizlife.rs/feed/",                             # Serbia – BizLife.rs
-
-    "https://www.poslovni.hr/rss",                          # Chorwacja – Poslovni dnevnik
-
-    "https://cin.ba/feed/",                                 # Bośnia i Hercegowina – CIN (sekcja także ekonomiczna)
-
-    "https://www.vijesti.me/rss/business",                  # Czarnogóra – Vijesti Business
-
-    "https://biznes.koha.net/rss",                          # Kosowo – Koha Biznes
-
-    "https://meta.mk/feed/",                                # Macedonia Północna – Meta.mk (analizy ekonomiczne)
-
-    "https://www.capital.bg/rss",                           # Bułgaria – Capital.bg :contentReference[oaicite:2]{index=2}
-
-    "https://www.monitor.si/rss/",                          # Słowenia – Monitor.si
-
-    "https://www.imf.org/external/index.xml"                # Globalnie – IMF Publications
-]
-
-
-DISCORD_WEBHOOK = "https://discordapp.com/api/webhooks/1392262052742959155/DEQ5zlgo3bdqzFkrLX1OyxyvybmRLnVNqcAQjeDVwt8FtUeXhCodvR6UuUILBdAUGvQi"  # <- tu wklej swój
-
-LT_ENDPOINTS = [
-    "https://libretranslate.com/translate",
-    "https://translate.astian.org/translate",
-    "https://libretranslate.de/translate",
-    "https://translate.argosopentech.com/translate"
-]
 
 from googletrans import Translator as GoogleTranslator
 google_translator = GoogleTranslator()
