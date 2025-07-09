87943f9123a0d9fd4af2d6ea221f3b05990663ac
Rarouken <rarouken@gmail.com>
2025-07-09 19:36:33 +0200

    Update rss_reader.py

--- rss_reader.py
+++ rss_reader.py
@@ -5,17 +5,95 @@ import os
 import time
 from transformers import pipeline
 
-# --- Lista krajów słowiańskich ---
-SLAVIC_COUNTRIES = [
-    # ...tu Twoja lista...
-]
-
 # --- Lista RSS-ów ---
 rss_feeds = [
-    # ...tu Twoja lista...
+    # 📌 Ogólne źródła informacyjne
+    "https://www.rmf24.pl/fakty/polska/feed",                # Polska – RMF24
+    "https://rss.onet.pl/",                                 # Polska – Onet
+    "https://gazeta.pl/rss/",                               # Polska – Gazeta.pl
+
+    "https://www.ceskatelevize.cz/rss/ct24.xml",            # Czechy – ČT24
+    "https://www.idnes.cz/rss.aspx",                        # Czechy – iDnes.cz
+    "https://rss.aktualne.cz/",                             # Czechy – Aktuálně.cz
+
+    "https://www.sme.sk/rss/",                              # Słowacja – SME.sk
+    "https://spravy.pravda.sk/rss/",                        # Słowacja – Pravda.sk
+    "https://www.aktuality.sk/rss/",                        # Słowacja – Aktuality.sk
+
+    "https://meduza.io/rss/all",                            # Rosja – Meduza
+    "https://rss.ria.ru/export/rss2/world/index.xml",       # Rosja – RIA Novosti
+    "https://www.themoscowtimes.com/rss",                   # Rosja – The Moscow Times
+
+    "https://www.pravda.com.ua/rss",                        # Ukraina – Ukrainska Pravda
+    "https://kyivindependent.com/feed",                     # Ukraina – Kyiv Independent
+    "https://www.kyivpost.com/rss/site.xml",                # Ukraina – Kyiv Post
+    "https://www.radiosvoboda.org/api/feeds",               # Ukraina – Radio Svoboda
+
+    "https://nashaniva.com/ru/rss/",                        # Białoruś – Nasha Niva
+    "https://www.rferl.org/api/feeds?region=Belarus",       # Białoruś – Radio Svoboda
+
+    "https://www.rts.rs/rss/ci.xml",                        # Serbia – RTS
+    "https://www.blic.rs/rss",                              # Serbia – Blic
+    "https://www.danas.rs/feed/",                           # Serbia – Danas
+
+    "https://www.hrt.hr/feeds/",                            # Chorwacja – HRT
+    "https://www.index.hr/rss/",                            # Chorwacja – Index.hr
+    "https://www.jutarnji.hr/rss",                          # Chorwacja – Jutarnji list
+
+    "https://balkaninsight.com/feed/",                      # Bośnia i Hercegowina – BIRN BiH
+    "https://cin.ba/feed/",                                 # Bośnia i Hercegowina – CIN
+    "https://ba.n1info.com/rss/",                           # Bośnia i Hercegowina – N1 BiH
+
+    "https://www.vijesti.me/rss",                           # Czarnogóra – Vijesti
+    "https://www.raskrinkavanje.me/rss",                    # Czarnogóra – Raskrinkavanje
+
+    "https://kallxo.com/feed/",                             # Kosowo – Kallxo
+    "https://www.koha.net/rss",                             # Kosowo – Koha Ditore
+
+    "https://meta.mk/feed/",                                # Macedonia Północna – Meta.mk
+    "https://www.slobodenpecat.mk/feed/",                   # Macedonia Północna – Sloboden Pechat
+
+    "https://btvnovinite.bg/rss",                           # Bułgaria – BTV News
+    "https://nova.bg/news/rss",                             # Bułgaria – Nova TV
+    "https://www.dnevnik.bg/rss.php",                       # Bułgaria – Dnevnik.bg
+
+    "https://www.rtvslo.si/rss/",                           # Słowenia – RTV Slovenija
+    "https://www.delo.si/rss/",                             # Słowenia – Delo
+    "https://podcrto.si/feed/",                             # Słowenia – Pod črto
+
+    # 💼 Sekcje ekonomiczne
+    "https://www.pb.pl/rss",                                # Polska – Puls Biznesu :contentReference[oaicite:1]{index=1}
+
+    "https://hn.ihned.cz/rss",                              # Czechy – Hospodářské noviny
+
+    "https://www.hnonline.sk/rss",                          # Słowacja – Hospodárske noviny
+
+    "https://www.vedomosti.ru/rss/news.xml",                # Rosja – Vedomosti
+
+    "https://www.interfax.com.ua/ua/rss/business/",         # Ukraina – Interfax-Ukraine Business
+
+    # Białoruś – brak RSS specyficznego, można sięgnąć po emigracyjne
+    "https://bizlife.rs/feed/",                             # Serbia – BizLife.rs
+
+    "https://www.poslovni.hr/rss",                          # Chorwacja – Poslovni dnevnik
+
+    "https://cin.ba/feed/",                                 # Bośnia i Hercegowina – CIN (sekcja także ekonomiczna)
+
+    "https://www.vijesti.me/rss/business",                  # Czarnogóra – Vijesti Business
+
+    "https://biznes.koha.net/rss",                          # Kosowo – Koha Biznes
+
+    "https://meta.mk/feed/",                                # Macedonia Północna – Meta.mk (analizy ekonomiczne)
+
+    "https://www.capital.bg/rss",                           # Bułgaria – Capital.bg :contentReference[oaicite:2]{index=2}
+
+    "https://www.monitor.si/rss/",                          # Słowenia – Monitor.si
+
+    "https://www.imf.org/external/index.xml"                # Globalnie – IMF Publications
 ]
 
-DISCORD_WEBHOOK = "https://discordapp.com/api/webhooks/..."  # <-- Twój webhook
+
+DISCORD_WEBHOOK = "https://discordapp.com/api/webhooks/1392262052742959155/DEQ5zlgo3bdqzFkrLX1OyxyvybmRLnVNqcAQjeDVwt8FtUeXhCodvR6UuUILBdAUGvQi"  # <- tu wklej swój
 
 LT_ENDPOINTS = [
     "https://libretranslate.com/translate",
